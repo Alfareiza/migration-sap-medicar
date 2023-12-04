b9d655e0f14c0bcf5e570d0bbc9d152f845cf0ac
Alfonso <alfareiza@gmail.com>
2023-12-04 20:08:28 -0300

    Recursos para maniobrar archivos pickles exportados del proyecto

---
+++ utils/pickles.py
@@ -0,0 +1,28 @@
+import json
+import pickle
+from core.settings import logger as log
+
+
+def load_pkl(pkl_path):
+    """ Given a pickle file, return his content """
+    with open(pkl_path, 'rb') as fp:
+        data = pickle.load(fp)
+    return data
+
+
+def get_json_from_pkl(pklpath, jsonfilepath=None):
+    """ Given a pickle file, create a json file with his content """
+    source = load_pkl(pklpath)
+
+    if not jsonfilepath:
+        jsonfilepath = f"{source.name}_exported.json"
+
+    with open(jsonfilepath, 'w', encoding='utf-8-sig') as jsonf:
+        log.info(f'Creando archivo {jsonfilepath}')
+        jsonf.write(json.dumps(source.data, indent=4, ensure_ascii=False))
+        log.info(f'Archivo {jsonfilepath} creado con éxito!')
+
+
+if __name__ == '__main__':
+    pathfile = '/Users/alfonso/Projects/migration-sap-medicar/dispensacion_data.pkl'
+    get_json_from_pkl(pathfile)
