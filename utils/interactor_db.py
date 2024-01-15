658cf4d702a175ac1a9b0f31dc472dcfab3e2e46
Alfonso <alfareiza@gmail.com>
2024-01-15 19:17:32 -0300

    Agregadas funciones para errores cuando export y mail. Guardando en bd masivamente

--- utils/interactor_db.py
+++ utils/interactor_db.py
@@ -1,40 +1,48 @@
 from typing import List
-
-from base.models import RegistroMigracion
+from base.models import RegistroMigracion, PayloadMigracion
 from utils.converters import Csv2Dict
 from utils.decorators import not_on_debug
 from core.settings import logger as log
 
 
 class DBHandler:
-    def __init__(self, migracion_id, filename, module_name, pk):
+
+    def __init__(self, migracion_id, module_name, pk):
         self.mig = RegistroMigracion.objects.get(id=migracion_id)
-        self.fname = filename
         self.mname = module_name
         self.ref = pk
-        self.registros = []
+        self.fname = ''
+        self.registros = None  # QuerySet
 
     def process(self, csvtodict: Csv2Dict):
-        log.info(f'[{self.mname}] guardando payloads en db')
-        # self.registros = self.create_objects(csvtodict)
-        # PayloadMigracion.objects.bulk_create(self.registros)
-        log.info(f'[{self.mname}] {len(self.registros)} payloads de archivo {self.mname} guardados en db')
+        log.info(f'[{self.mname}] guardando {len(csvtodict.data)} payloads en db')
+        objs = self.create_objects(csvtodict)
+        try:
+            PayloadMigracion.objects.bulk_create(objs)
+            self.registros = PayloadMigracion.objects.filter(id__in=[p.id for p in objs])
+        except Exception as e:
+            log.error(f"Error {e} al guardar en db")
+        else:
+            log.info(f'[{self.mname}] {len(self.registros)} payloads de archivo {self.mname} guardados en db')
 
-    # def create_objects(self, info: Csv2Dict) -> List[PayloadMigracion]:
-    #     """ Crea los PayloadMigracion con base en los payloads
-    #     procesados previamente por ProcessCSV. """
-    #     res = []
-    #     for k in info.succss:
-    #         payload = PayloadMigracion(migracion_id=self.mig,
-    #                                    nombre_archivo=self.fname,
-    #                                    documento_referencia=self.ref,
-    #                                    valor_referencia=k,
-    #                                    modulo=self.mname,
-    #                                    cantidad_lineas=len(info.data[k]['csv']),
-    #                                    lineas=info.data[k]['csv'],
-    #                                    payload=info.data[k]['json'])
-    #         res.append(payload)
-    #     return res
+    def create_objects(self, info: Csv2Dict) -> List[PayloadMigracion]:
+        """Crea los PayloadMigracion con base en los payloads
+        procesados previamente por ProcessCSV. """
+        res = []
+        for k in info.data:
+            payload = PayloadMigracion(
+                status=info.data[k]['csv'][0]['Status'],
+                migracion_id=self.mig,
+                modulo=self.mname,
+                ref_documento=self.ref,
+                valor_documento=k,
+                nombre_archivo=self.fname,
+                cantidad_lineas_documento=len(info.data[k]['csv']),
+                payload=info.data[k]['json'],
+                lineas=info.data[k]['csv'],
+            )
+            res.append(payload)
+        return res
 
 
 @not_on_debug
@@ -66,6 +74,18 @@ def update_estado_error_drive(migracion_id: int) -> None:
 def update_estado_error_sap(migracion_id: int) -> None:
     migracion = RegistroMigracion.objects.get(id=migracion_id)
     update_estado(migracion, 'error sap')
+
+
+@not_on_debug
+def update_estado_error_export(migracion_id: int) -> None:
+    migracion = RegistroMigracion.objects.get(id=migracion_id)
+    update_estado(migracion, 'error export')
+
+
+@not_on_debug
+def update_estado_error_mail(migracion_id: int) -> None:
+    migracion = RegistroMigracion.objects.get(id=migracion_id)
+    update_estado(migracion, 'error mail')
 
 
 @not_on_debug
